e3b84f2c584c2b34daaa3bcaa2c8d84df9cff9f5
zeeshan <zeeshanshafiq84>
2023-05-17 15:57:36 -0700

    =fourht commit

---
+++ script.js
@@ -0,0 +1,18 @@
+window.getWeather = function(){
+
+    let cityName = document.querySelector ("#cityName").value;
+    
+   let API_KEY = '32676405e39e746c7f7746f3491194bd'
+   
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    document.querySelector("#result").innerHTML = "weather data successfully retrived"
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error.data);
+    document.querySelector("#result").innerHTML = "error in getting weather data"
+  })
+}
