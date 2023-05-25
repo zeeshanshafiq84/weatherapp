1d1a8040c4455082bf09ec088453f099f5bdd122
zeeshan <zeeshanshafiq84>
2023-05-25 04:51:15 -0700

    =third commit

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-   window.getWeather = function(){
+    window.getWeather = function () {
 
-    let cityName = document.querySelector ("#cityName").value;
-    
-   let API_KEY = '32676405e39e746c7f7746f3491194bd'
-   
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
+    let cityName = document.querySelector("#cityName").value;
+
+    let API_KEY = '32676405e39e746c7f7746f3491194bd'
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
   .then(function (response) {
     // handle success
     console.log(response.data);
-    document.querySelector("#result").innerHTML = "weather data successfully retrived"
-  })
+    document.querySelector('#result').innerHTML = `current Temprature of ${response.data.name} is ${response.data.main.temp}°C`
+    document.querySelector('#humidity').innerHTML = `Humidity <br/> ${response.data.main.humidity}`
+
+})
   .catch(function (error) {
     // handle error
     console.log(error.data);
-    document.querySelector("#result").innerHTML = "error in getting weather data"
+    document.querySelector("#result").innerHTML = "Error in Getting Weather Data"
   })
 }
